b30f05f960433a3459b87daa5980bdd0a7f72168
zeyadsharo <33178745+zeyadsharo@users.noreply.github.com>
2020-05-01 07:44:34 +0300

    update navbar and admin

--- resources/js/components/Admin.vue
+++ resources/js/components/Admin.vue
@@ -1,29 +1,70 @@
-<template v-slot:activator="{ on }">
-  <div id="app">
-    <v-app id="inspire" >
-      <v-navigation-drawer clipped fixed v-model="drawer" app>
-        <sidebar></sidebar>
-      </v-navigation-drawer>
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" :clipped="$vuetify.breakpoint.lgAndUp" app>
+      <v-list dense>
+          <sidebar></sidebar>
+      </v-list>
+    </v-navigation-drawer>
+    <navbar :user="user">
 
-      <navbar :user="user"></navbar>
 
-      <v-content>
-        <v-container fluid fill-height>
-          <v-layout justify-center>
+
+    </navbar>
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+        
+              <v-layout justify-center>
             <v-flex shrink>
               <router-view></router-view>
             </v-flex>
           </v-layout>
+           
+        </v-row>
+      </v-container>
+    </v-content>
+    <v-btn bottom color="pink" dark fab fixed right @click="dialog = !dialog">
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
+    <v-dialog v-model="dialog" width="800px">
+      <v-card>
+        <v-card-title class="grey darken-2">Create contact</v-card-title>
+        <v-container>
+          <v-row class="mx-2">
+            <v-col class="align-center justify-space-between" cols="12">
+              <v-row align="center" class="mr-0">
+                <v-avatar size="40px" class="mx-3">
+                  <img src="//ssl.gstatic.com/s2/oz/images/sge/grey_silhouette.png" alt />
+                </v-avatar>
+                <v-text-field placeholder="Name" />
+              </v-row>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field prepend-icon="mdi-account-card-details-outline" placeholder="Company" />
+            </v-col>
+            <v-col cols="6">
+              <v-text-field placeholder="Job title" />
+            </v-col>
+            <v-col cols="12">
+              <v-text-field prepend-icon="mdi-mail" placeholder="Email" />
+            </v-col>
+            <v-col cols="12">
+              <v-text-field type="tel" prepend-icon="mdi-phone" placeholder="(000) 000 - 0000" />
+            </v-col>
+            <v-col cols="12">
+              <v-text-field prepend-icon="mdi-text" placeholder="Notes" />
+            </v-col>
+          </v-row>
         </v-container>
-      </v-content>
-
-      <v-footer color="primary darken-1" app fixed>
-        <v-spacer></v-spacer>
-        <span>&copy; 2019</span>
-        <v-spacer></v-spacer>
-      </v-footer>
-    </v-app>
-  </div>
+        <v-card-actions>
+          <v-btn text color="primary">More</v-btn>
+          <v-spacer />
+          <v-btn text color="primary" @click="dialog = false">Cancel</v-btn>
+          <v-btn text @click="dialog = false">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-app>
 </template>
 
 <script>
@@ -31,8 +72,13 @@ import Sidebar from "./_sidebar";
 import Navbar from "./_navbar";
 export default {
   components: { Sidebar, Navbar },
+  props: {
+    source: String
+  },
   data: () => ({
-    drawer: null
+    dialog: false,
+    drawer: null,
+   
   }),
   props: ["user"]
 };
--- resources/js/components/_navbar.vue
+++ resources/js/components/_navbar.vue
@@ -1,57 +1,87 @@
 <template v-slot:activator="{ on }">
-  <v-app-bar  color="primary darken-1" dart app fixed clipped-left>
-    <v-app-bar-nav-icon  color="red" @click="drawer = true"></v-app-bar-nav-icon>
-     <v-toolbar-title>Maintenance Admin</v-toolbar-title>
-    <v-spacer></v-spacer>
+  <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="blue darken-3" dark>
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+    <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
+      <span class="hidden-sm-and-down">Maintenance Admin</span>
+    </v-toolbar-title>
+    <v-text-field
+      flat
+      solo-inverted
+      hide-details
+      prepend-inner-icon="mdi-magnify"
+      label="Search"
+      class="hidden-sm-and-down"
+    />
+    <v-spacer />
 
-    <v-menu
-      offset-y
-      origin="center center"
-      class="elelvation-1"
-      :nudge-bottom="14"
-      transition="scale-transition"
-    >
-      <v-btn color="red" @click="markAsRead" icon text >
-        <v-badge color="red" overlap>
+    <v-menu left bottom>
+      <template v-slot:activator="{ on }">
+        <v-btn icon v-on="on" @click="markAsRead" text>
+          <!-- <v-badge color="red" overlap>
           <span slot="badge">{{unreadNotifications.length}}</span>
           <v-icon medium>far fa-bell</v-icon>
-        </v-badge>
-        
-      </v-btn>
+          </v-badge>-->
+          <v-badge
+            :content="unreadNotifications.length"
+            :value="unreadNotifications.length"
+            color="red"
+            overlap
+          >
+            <v-icon>mdi-bell</v-icon>
+          </v-badge>
+        </v-btn>
+      </template>
 
       <v-list>
-        <v-list-tile :class="{'green': notification.read_at==null}" @click="markAsRead" v-for="notification in allNotifications" :key="notification.id">
-          <v-list-tile-content>
-            <v-list-tile-title>{{notification.data.createdUser.name}} has just registered on {{notification.created_at}}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
+        <v-list-item
+          :class="{'green': notification.read_at==null}"
+          @click="markAsRead"
+          v-for="notification in allNotifications"
+          :key="notification.id"
+        >
+          <v-list-item-title>{{notification.data.createdUser.name}} has just registered on {{notification.created_at}}</v-list-item-title>
+        </v-list-item>
       </v-list>
     </v-menu>
-    <v-menu offset-y origin="center center" :nudge-bottom="10" transition="scale-transition">
-      <v-btn icon large text >
-        <v-avatar size="30px">
-          <img  src="https://via.placeholder.com/150" alt="Michael Wang">
-        </v-avatar>
-      </v-btn>
-      <v-list class="pa-0">
-        <v-list-tile ripple="ripple" rel="noopener">
-          <v-list-tile-content>
-            <v-list-tile-title>{{user.name}}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
+    <v-menu left bottom>
+      <template v-slot:activator="{ on }">
+        <v-btn icon v-on="on" @click="markAsRead" text>
+          <!-- <v-badge color="red" overlap>
+          <span slot="badge">{{unreadNotifications.length}}</span>
+          <v-icon medium>far fa-bell</v-icon>
+          </v-badge>-->
+          <v-badge
+            :content="unreadNotifications.length"
+            :value="unreadNotifications.length"
+            color="red"
+            overlap
+          >
+            <v-icon>far fa-user</v-icon>
+          </v-badge>
+        </v-btn>
+      </template>
 
-      <v-list class="pa-0">
-        <v-list-tile @click="logout" ripple="ripple" rel="noopener">
-          <v-list-tile-action>
-            <v-icon color="red">fas fa-power-off</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Logout</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
+      <v-list>
+        <v-list-item>
+          <v-list-item-title>{{user.name}}</v-list-item-title>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-title @click="logout" ripple="ripple" rel="noopener">
+            <v-list-item-action>
+              <v-icon color="red">fas fa-power-off </v-icon> 
+            </v-list-item-action>
+           Logout
+          </v-list-item-title>
+
+        </v-list-item>
       </v-list>
     </v-menu>
+
+    <!-- <v-btn icon large>
+      <v-avatar size="32px" item>
+        <v-img src="https://cdn.vuetifyjs.com/images/logos/logo.svg" alt="Vuetify" />
+      </v-avatar>
+    </v-btn>-->
   </v-app-bar>
 </template>
 
@@ -60,17 +90,17 @@
 export default {
   props: ["user"],
   data: () => ({
-    drawer: false,
+    drawer: true,
     allNotifications: [],
-    unreadNotifications: [],
+    unreadNotifications: []
   }),
   props: ["user"],
-  watch:{
-      allNotifications(val){
-          this.unreadNotifications =  this.allNotifications.filter(notification => {
-            return notification.read_at == null;
-        });
-      }
+  watch: {
+    allNotifications(val) {
+      this.unreadNotifications = this.allNotifications.filter(notification => {
+        return notification.read_at == null;
+      });
+    }
   },
 
   methods: {
@@ -78,18 +108,18 @@ export default {
       axios.post("/logout").then(response => window.location.reload());
     },
     markAsRead() {
-      axios.get("/mark-all-read/" + this.user.id).then(response=>{
-          this.unreadNotifications = [];
+      axios.get("/mark-all-read/" + this.user.id).then(response => {
+        this.unreadNotifications = [];
       });
     }
   },
- 
+
   created() {
     this.allNotifications = this.user.notifications;
 
-    this.unreadNotifications =  this.allNotifications.filter(notification => {
-        return notification.read_at == null;
-      });
+    this.unreadNotifications = this.allNotifications.filter(notification => {
+      return notification.read_at == null;
+    });
 
     // Echo.private("App.User." + this.user.id).notification(notification => {
     //   this.allNotifications.unshift(notification.notification);
