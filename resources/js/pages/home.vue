e328e1a5c793e4f192106905a69350e972f355b8
zeyadsharo <33178745+zeyadsharo@users.noreply.github.com>
2020-05-14 16:59:53 +0300

    update home page

--- resources/js/pages/home.vue
+++ resources/js/pages/home.vue
@@ -3,11 +3,18 @@
     <v-app id="inspire" light>
       <v-card>
         <v-toolbar color="cyan" dark flat>
+          <div class="mx-3"></div>
+
+          <v-badge bordered bottom color="deep-purple accent-4" dot offset-x="10" offset-y="10">
+            <v-avatar    size="40">
+              <v-img src="https://cdn.vuetifyjs.com/images/lists/2.jpg"></v-img>
+            </v-avatar>
+     
+          </v-badge>       
+          Zeyad Sharo
           <v-spacer></v-spacer>
           <v-toolbar-title>Maintenace System Home</v-toolbar-title>
-
           <v-spacer></v-spacer>
-
           <!-- <v-btn icon>
             <v-icon>mdi-magnify</v-icon>
           </v-btn>-->
@@ -17,22 +24,22 @@
           </v-btn>
 
           <template v-slot:extension>
-            <v-tabs v-model="activeTab"  centered slider-color="yellow">
+            <v-tabs v-model="activeTab" centered slider-color="yellow">
               <v-tab v-for="tab in tabs" :key="tab.id" :to="tab.route" exact>{{ tab.name }}</v-tab>
             </v-tabs>
           </template>
         </v-toolbar>
       </v-card>
 
-        <v-content>
-      <v-container class="fill-height" fluid>
-         <v-layout justify-center>
+      <v-content>
+        <v-container class="fill-height" fluid>
+          <v-layout justify-center>
             <v-flex shrink>
               <router-view></router-view>
             </v-flex>
           </v-layout>
-      </v-container>
-    </v-content>
+        </v-container>
+      </v-content>
       <v-footer color="primary lighten-1" padless>
         <v-row justify="center" no-gutters>
           <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
@@ -52,11 +59,11 @@ export default {
   },
   data: () => ({
     drawer: null,
-      activeTab: `/home/requests`,
+    activeTab: `/home/requests`,
     tabs: [
       { id: 1, name: "Requests", route: `/home/requests` },
       { id: 2, name: "History", route: `/home/history` },
-       { id: 3, name: "in Process", route: `/home/inprogress` }
+      { id: 3, name: "in Process", route: `/home/inprogress` }
     ]
   }),
   methods: {
--- resources/js/workflow/requests.vue
+++ resources/js/workflow/requests.vue
@@ -1,99 +1,125 @@
 <template>
-  <v-form
-    ref="form"
-    v-model="valid"
-    lazy-validation
-  >
-    <v-text-field
-      v-model="name"
-      :counter="10"
-      :rules="nameRules"
-      label="Name"
-      required
-    ></v-text-field>
-
-    <v-text-field
-      v-model="email"
-      :rules="emailRules"
-      label="E-mail"
-      required
-    ></v-text-field>
-
-    <v-select
-      v-model="select"
-      :items="items"
-      :rules="[v => !!v || 'Item is required']"
-      label="Item"
-      required
-    ></v-select>
-
-    <v-checkbox
-      v-model="checkbox"
-      :rules="[v => !!v || 'You must agree to continue!']"
-      label="Do you agree?"
-      required
-    ></v-checkbox>
-
-    <v-btn
-      :disabled="!valid"
-      color="success"
-      class="mr-4"
-      @click="validate"
-    >
-      Validate
-    </v-btn>
-
-    <v-btn
-      color="error"
-      class="mr-4"
-      @click="reset"
-    >
-      Reset Form
-    </v-btn>
-
-    <v-btn
-      color="warning"
-      @click="resetValidation"
-    >
-      Reset Validation
-    </v-btn>
-  </v-form>
+  <v-card>
+    <v-card-title>
+      Nutrition
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :search="search"
+    ></v-data-table>
+  </v-card>
 </template>
 
 <script>
   export default {
-    data: () => ({
-      valid: true,
-      name: '',
-      nameRules: [
-        v => !!v || 'Name is required',
-        v => (v && v.length <= 10) || 'Name must be less than 10 characters',
-      ],
-      email: '',
-      emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-      ],
-      select: null,
-      items: [
-        'Item 1',
-        'Item 2',
-        'Item 3',
-        'Item 4',
-      ],
-      checkbox: false,
-    }),
-
-    methods: {
-      validate () {
-        this.$refs.form.validate()
-      },
-      reset () {
-        this.$refs.form.reset()
-      },
-      resetValidation () {
-        this.$refs.form.resetValidation()
-      },
+    data () {
+      return {
+        search: '',
+        headers: [
+          {
+            text: 'Dessert (100g serving)',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Calories', value: 'calories' },
+          { text: 'Fat (g)', value: 'fat' },
+          { text: 'Carbs (g)', value: 'carbs' },
+          { text: 'Protein (g)', value: 'protein' },
+          { text: 'Iron (%)', value: 'iron' },
+        ],
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+            iron: '1%',
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+            iron: '1%',
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+            iron: '7%',
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3,
+            iron: '8%',
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+            fat: 16.0,
+            carbs: 49,
+            protein: 3.9,
+            iron: '16%',
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0,
+            iron: '0%',
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0,
+            iron: '2%',
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+            fat: 3.2,
+            carbs: 87,
+            protein: 6.5,
+            iron: '45%',
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+            fat: 25.0,
+            carbs: 51,
+            protein: 4.9,
+            iron: '22%',
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7,
+            iron: '6%',
+          },
+        ],
+      }
     },
   }
 </script>
